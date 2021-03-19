6ea3da52e6f86f5fa1a034cb22523b01e029f849
Gabriel <souteramoto@gmail.com>
2021-03-19 14:24:40 -0300

    recarregando até o login

---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ab7645a3d4e987640aed79a4a25f932366fc87e0',
+    'name' => 'souzagabi/ecommerce',
+  ),
+  'versions' => 
+  array (
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v5.2.22',
+      'version' => '5.2.22.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b18cb98131bd83103ccb26a888fdfe3177b8a663',
+    ),
+    'rain/raintpl' => 
+    array (
+      'pretty_version' => '3.0.0',
+      'version' => '3.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5bfdf4602b8adb0e80b222fa29ac559aa9c06abf',
+    ),
+    'slim/slim' => 
+    array (
+      'pretty_version' => '2.0.0',
+      'version' => '2.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3e95c9abbc57a8f023ab4dacebcab9dae4d9f1f0',
+    ),
+    'souzagabi/ecommerce' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ab7645a3d4e987640aed79a4a25f932366fc87e0',
+    ),
+  ),
+);
---
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -0,0 +1,12 @@
+<?php
+    namespace HCode;
+    use \Hcode\DB\Sql;
+    
+    class User {
+        public static function login($login, $password){
+            $sql = new Sql();
+        }
+    }
+    
+
+?>
